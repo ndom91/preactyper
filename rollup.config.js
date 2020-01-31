9ce3b4577fdc97913b1b4448de865f78ae5a4b72
ndo@ndo1 <yo@ni.co.de>
2020-01-31 15:10:54 +0100

    update: rollup config + version bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,41 @@
-import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss';
-import commonjs from 'rollup-plugin-commonjs';
-import autoprefixer from 'autoprefixer';
-import buble from 'rollup-plugin-buble';
-import { terser } from 'rollup-plugin-terser';
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import commonjs from "rollup-plugin-commonjs";
+import autoprefixer from "autoprefixer";
+import buble from "rollup-plugin-buble";
+import { terser } from "rollup-plugin-terser";
 
-import pkg from './package.json';
+import pkg from "./package.json";
 
 const getDependencies = ({ dependencies = {}, peerDependencies = {} }) => [
   ...Object.keys(dependencies),
-  ...Object.keys(peerDependencies),
+  ...Object.keys(peerDependencies)
 ];
 
 const base = {
-  input: 'src/index.tsx',
+  input: "src/index.tsx",
   plugins: [
     typescript({
-      typescript: require('typescript'),
-      objectHashIgnoreUnknownHack: true,
+      typescript: require("typescript"),
+      objectHashIgnoreUnknownHack: true
     }),
     buble({
-			jsx: 'h',
-      objectAssign: 'Object.assign',
-      exclude: '**/*.css'
+      jsx: "h",
+      objectAssign: "Object.assign",
+      exclude: "**/*.css"
     }),
     commonjs(),
-		resolve(),
+    resolve(),
     postcss({
-      plugins: [
-        autoprefixer({ browsers: ['last 2 versions'] }),
-      ],
+      plugins: [autoprefixer({ browsers: ["last 2 versions"] })]
     }),
     terser({
       mangle: {
-        reserved: ['Typer', 'Character', 'Caret'],
-      },
-    }),
-  ],
+        reserved: ["Typer", "Character", "Caret"]
+      }
+    })
+  ]
 };
 
 export default [
@@ -45,20 +43,21 @@ export default [
     ...base,
     output: {
       file: pkg.module,
-      format: 'esm',
+      format: "esm"
     },
-    external: getDependencies(pkg),
+    external: getDependencies(pkg)
   },
   {
     ...base,
     output: {
-      file: 'dist/reactyper.min.js',
-      format: 'esm',
+      file: "dist/preactyper.min.js",
+      format: "esm",
       globals: {
-        react: 'React',
-        'lodash.split': '_.split',
+        react: "React",
+        "lodash.split": "_.split"
       },
-      name: 'Typer',
+      name: "Typer"
     }
   }
 ];
+
